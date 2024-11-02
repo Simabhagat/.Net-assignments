e7928f5334ffc9b0b293bca31f3457a2e5fcecce
Simabhagat <moirangthembhagat338@gmail.com>
2024-11-02 17:36:21 +0530

    added constructors

---
+++ constructors/withCSharp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConstructorExample
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Default constructor
+        public Person()
+        {
+            Name = "Unknown";
+            Age = 0;
+            Console.WriteLine("Default constructor called");
+        }
+
+        // Parameterized constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine("Parameterized constructor called");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Using the default constructor
+            Person person1 = new Person();
+            person1.Display();
+
+            // Using the parameterized constructor
+            Person person2 = new Person("Alice", 25);
+            person2.Display();
+        }
+    }
+}
---
+++ constructors/withVB.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+
+Namespace ConstructorExample
+
+    Public Class Person
+        Public Property Name As String
+        Public Property Age As Integer
+
+        ' Default constructor
+        Public Sub New()
+            Name = "Unknown"
+            Age = 0
+            Console.WriteLine("Default constructor called")
+        End Sub
+
+        ' Parameterized constructor
+        Public Sub New(name As String, age As Integer)
+            Me.Name = name
+            Me.Age = age
+            Console.WriteLine("Parameterized constructor called")
+        End Sub
+
+        Public Sub Display()
+            Console.WriteLine($"Name: {Name}, Age: {Age}")
+        End Sub
+    End Class
+
+    Module Program
+        Sub Main()
+            ' Using the default constructor
+            Dim person1 As New Person()
+            person1.Display()
+
+            ' Using the parameterized constructor
+            Dim person2 As New Person("Alice", 25)
+            person2.Display()
+        End Sub
+    End Module
+End Namespace
